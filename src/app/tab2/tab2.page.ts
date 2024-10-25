224b5572ee4334b57eaf370aa300433b94be8caf
Miguel <miguel.carvajal@epn.edu.ec>
2024-10-25 14:57:32 -0500

    Pestaña Dos

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { ItemReorderEventDetail } from '@ionic/angular';
-import { addIcons } from 'ionicons';
-import { pizza } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab2',
@@ -9,48 +6,14 @@ import { pizza } from 'ionicons/icons';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  isAlertOpen = false;
-  alertHeader: string = '';  
-  alertSubHeader: string = ''; 
-  alertMessage: string = '';  
 
   constructor() {
 
-    addIcons({ pizza });
+
   }
 
   irAGithub() {
     window.open('https://github.com/Miguel2314carvajal', '_blank'); 
   }
 
-  handleReorder(ev: CustomEvent<ItemReorderEventDetail>) {
-    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
-    ev.detail.complete()
-  }
-
-
-  setOpen(isOpen: boolean) {
-    this.isAlertOpen = isOpen;
-  }
-
-  abrirPopup(tipo: string) {
-    switch (tipo) {
-      case 'document':
-        this.alertHeader = 'Hobbys';
-        this.alertSubHeader = 'Deportes Favoritos';
-        this.alertMessage = 'Futbol, Basket, Futsala, KickBoxing, Boxeo';
-        break;
-      case 'color-palette':
-        this.alertHeader = 'Colores Favoritos';
-        this.alertSubHeader = 'Selección de colores';
-        this.alertMessage = 'Azul, Rojo y Negro';
-        break;
-      case 'globe':
-        this.alertHeader = 'Lugares';
-        this.alertSubHeader = 'Mis favoritos';
-        this.alertMessage = 'Parques Botanicos, Volcanes, Montañas';
-        break;
-    }
-    this.setOpen(true);
-  }
 }
